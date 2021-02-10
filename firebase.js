a2acd92e49dc353f4138771e2d575810712d7a8b
Mehmet Faruk Türkoğlu <70632280+yussems@users.noreply.github.com>
2021-02-10 04:23:53 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAes3VF1E8MNXxhvZOUcNv_eA2gu9yeP3Y",
+  apiKey: "AIzaSyAes3VF1E8MNXxhvZOUcNv_eA2gu9dyeP3Y",
   authDomain: "possible-post-298309.firebaseapp.com",
   projectId: "possible-post-298309",
-  storageBucket: "possible-post-298309.appspot.com",
+  storageBucket: "possible-post-29830d9.appspot.com",
   messagingSenderId: "503291017399",
-  appId: "1:503291017399:web:ac1cbcee70102c532c7532",
+  appId: "1:503291017399:web:ac1cbcee7010d2c532c753d2",
 };
 
 let app;
